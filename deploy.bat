5e3316fe37adb6a14ce32872ea183e3a4cf2b62a
vergil0210 <845800056@qq.com>
2021-07-14 17:16:53 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,6 @@ cd ../dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/vergil0210/blog.git master:gh-pages
+git push -f https://github.com/vergil0210/blog.git master
 pause
 
